7d35869ca527b433dd34d786c6269ed8df5970ed
David Wolff <dcwolff218@gmail.com>
2020-02-04 04:09:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,10 +9,15 @@ def find_element_index(array, value_to_find)
    nil
 end
 
+#def find_max_value(array)
+ # max = array.sort.last
+ # return max
+#end
+
 def find_max_value(array)
-  max = array.sort.last
-  return max
+  
 end
+
 
 def find_min_value(array)
   min = array.sort.first
